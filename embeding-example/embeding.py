a3d4a6cd87c0a3ebcdb996905422851ed7db3a57
Fournet Enzo <enzofournet31@gmail.com>
2024-11-27 11:44:42 +0100

    Update embeding.py

--- embeding-example/embeding.py
+++ embeding-example/embeding.py
@@ -5,10 +5,11 @@ prompt = "J'adore manger du fromage à raclette."
 
 # Récupère les embeddings pour le prompt
 embeddings = ollama.embeddings(
-  model='nomic-embed-text',
-  prompt=prompt,
+    model="nomic-embed-text",
+    prompt=prompt,
 )
 
 # Affichage du résultat de l'embedding pour le prompt
 print(f"Embeddings for '{prompt}':")
 print(embeddings.embedding)
+print(f"Dimensions: {len(embeddings.embedding)}")
